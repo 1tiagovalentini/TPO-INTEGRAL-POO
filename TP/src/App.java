0bcf0d72b37e4b21232adf5ef99d580d519f07f4
Ivan Lautaro Sanchez <100932334+IvanLSanchez@users.noreply.github.com>
2024-10-30 19:10:25 -0300

    Update App.java

--- TP/src/App.java
+++ TP/src/App.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ingrese el número de la acción a realizar: \n" +
-        "1.Crear un nuevo evento\n" + 
-        "2.Modificar un evento\n" +
-        "3.Ver eventos\n" + 
-        "4.Ver registro de personas de cierto evento\n" +
-        "5.Inscribir una persona a un evento");
+        System.out.println("Ingrese el número de la acción a realizar: \n
+        1.Crear un nuevo evento\n
+        2.Modificar un evento\n
+        3.Ver eventos\n
+        4.Ver registro de personas de cierto evento\n
+        5.Inscribir una persona a un evento");
 
 
         Scanner input = new Scanner(System.in);
+        
         do {
             System.out.println("Ingrese numero:");
             opcion = Integer.parseInt(input.nextLine());
