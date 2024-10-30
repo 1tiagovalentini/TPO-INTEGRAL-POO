ffb2ebb24dce891d9d49c7b7849e743fe8684c4a
Tomas Liñeira Abal <tlineira@gmail.com>
2024-10-30 19:17:41 -0300

    Update App.java
    m

--- TP/src/App.java
+++ TP/src/App.java
@@ -2,19 +2,29 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ingrese el número de la acción a realizar: \n
-        1.Crear un nuevo evento\n
-        2.Modificar un evento\n
-        3.Ver eventos\n
-        4.Ver registro de personas de cierto evento\n
-        5.Inscribir una persona a un evento");
 
+        System.out.println("Ingrese el número de la acción a realizar: \n" +
+        "1.Crear un nuevo evento\n" + 
+        "2.Modificar un evento\n" +
+        "3.Ver eventos\n" + 
+        "4.Ver registro de personas de cierto evento\n" +
+        "5.Inscribir una persona a un evento" +
+        "6.Gestionar recursos" +
+        "7.Ver calendario");
 
-        Scanner input = new Scanner(System.in);
         
-        do {
-            System.out.println("Ingrese numero:");
-            opcion = Integer.parseInt(input.nextLine());
-        } while (opcion < 1 || opcion > 4);
+        int opcion;
+        Scanner input = new Scanner(System.in);
+
+        try{
+            do {
+                System.out.println("Ingrese numero:");
+                opcion = Integer.parseInt(input.nextLine());
+            } while (opcion < 1 || opcion > 5);
+        
+        }
+        finally{
+            input.close();
+        }
     }
 }
