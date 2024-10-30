332ee97a08d3bf83701def2c0286ec63256c33af
Nicolas Lussoro <lussoro8@gmail.com>
2024-10-30 19:06:53 -0300

    Añado Clases Persona y Recurso

--- TP/src/Persona.java
+++ TP/src/Persona.java
@@ -1,3 +1,17 @@
 public class Persona {
+    private String nombre;
+    private String feedbackEvento;
 
+    public Persona(String nombre, String feedbackEvento) {
+        this.nombre = nombre;
+        this.feedbackEvento = feedbackEvento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getFeedbackEvento() {
+        return feedbackEvento;
+    }
 }
