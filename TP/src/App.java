8f507c98ee269aebcede6412cfd87388e55e3a69
Tomas Liñeira Abal <tlineira@gmail.com>
2024-10-30 19:07:24 -0300

    Update App.java
    main

--- TP/src/App.java
+++ TP/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Ingrese el número de la acción a realizar: \n" +
+        "1.Crear un nuevo evento\n" + 
+        "2.Modificar un evento\n" +
+        "3.Ver eventos\n" + 
+        "4.Ver registro de personas de cierto evento\n" +
+        "5.Inscribir una persona a un evento");
+
+
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.println("Ingrese numero:");
+            opcion = Integer.parseInt(input.nextLine());
+        } while (opcion < 1 || opcion > 4);
     }
 }
