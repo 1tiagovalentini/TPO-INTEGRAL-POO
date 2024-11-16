3f98ef4b214e3601f1ee90df493ab92bd085379b
Ivan Lautaro Sanchez <100932334+IvanLSanchez@users.noreply.github.com>
2024-11-15 23:24:41 -0300

    eliminar notificaciones

--- TP/src/Persona.java
+++ TP/src/Persona.java
@@ -2,18 +2,9 @@ import java.util.ArrayList;
 
 public class Persona {
     private String nombre;
-    private ArrayList<String> notificaciones;
 
     public Persona(String nombre) {
         this.nombre = nombre;
-    }
-
-    public void setNotificacion(String notificacion) {
-        this.notificaciones.add(notificacion);
-    }
-
-    public ArrayList<String> getNotificacion() {
-        return notificaciones;
     }
 
     public String getNombre() {
