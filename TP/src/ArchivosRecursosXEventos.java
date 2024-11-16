534d0051b2f8d8f46a1966960c97d028928716bc
Ivan Lautaro Sanchez <100932334+IvanLSanchez@users.noreply.github.com>
2024-11-16 12:02:55 -0300

    update

--- TP/src/ArchivosRecursosXEventos.java
+++ TP/src/ArchivosRecursosXEventos.java
@@ -32,6 +32,32 @@ public class ArchivosRecursosXEventos extends GestorDeArchivos{
         }
     }
 
+    public void modificarArchivo(String idEvento, String datoAModificar){
+        String archivoModificado = "";
+        try{
+            FileReader lector = new FileReader(archivo);
+            BufferedReader lectura = new BufferedReader(lector);
+            String linea = lectura.readLine();
+            while(linea!=null){
+                String[] datos = linea.split(",");
+                if(datos[0].equals(idEvento)){
+                    archivoModificado = archivoModificado + datoAModificar + "\n";
+                }else{
+                    archivoModificado = archivoModificado + linea + "\n";
+                }
+                linea = lectura.readLine();
+            }
+            lectura.close();
+
+            FileWriter escritura = new FileWriter(archivo);
+            escritura.write(archivoModificado);
+            escritura.close();
+
+        }catch(IOException excepcion){
+            System.out.println(excepcion);
+        }
+    }
+
     @Override
     protected void cargarEnMemoria(String[] datos, GestorDeEventos gestor) {
         gestor.agregarRecurso(datos[1], datos[0], true);
